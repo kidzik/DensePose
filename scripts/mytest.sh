81e296b688959d7e9a94ced7c8030ab96db64251
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2018-10-22 14:26:55 -0700

    jpg instead of png

--- scripts/mytest.sh
+++ scripts/mytest.sh
@@ -1,6 +1,6 @@
 python2 scripts/infer_simple.py \
 	--cfg configs/DensePose_ResNet101_FPN_s1x-e2e.yaml \
 	--output-dir out/ \
-	--image-ext png \
+	--image-ext jpg \
 	--wts DensePoseData/models/DensePose_ResNet101_FPN_s1x-e2e.pkl \
 	frames/
